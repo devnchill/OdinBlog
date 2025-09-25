b8cd41a3040c9d26c8eca783529df906b742fcd4
0xDev <169875752+devnchill@users.noreply.github.com>
2025-09-25 23:37:35 +0530

    chore: setting up frontend-viewer

---
+++ frontend-viewer/src/pages/SignUpScreen.tsx
@@ -0,0 +1,5 @@
+const SignUpScreen = () => {
+  return <div>SignUp</div>;
+};
+
+export default SignUpScreen;
--- frontend-viewer/src/routes.tsx
+++ frontend-viewer/src/routes.tsx
@@ -1,9 +1,20 @@
 import { createBrowserRouter } from "react-router";
+import SignUpScreen from "./pages/SignUpScreen";
+import LoginScreen from "./pages/LoginScreen";
+import HomeScreen from "./pages/HomeScreen";
 
 const router = createBrowserRouter([
   {
+    path: "/signup",
+    element: <SignUpScreen />,
+  },
+  {
+    path: "/login",
+    element: <LoginScreen />,
+  },
+  {
     path: "/",
-    element: <div>Hello World</div>,
+    element: <HomeScreen />,
   },
 ]);
 
