03a7e6d18f814941576eed7a81a93c1ad93dcbca
Viena <169875752+devnchill@users.noreply.github.com>
2026-02-28 13:21:59 +0530

    refactor: throw error incase of undefined DATABASE_URL

--- apps/backend/lib/prisma.ts
+++ apps/backend/lib/prisma.ts
@@ -2,6 +2,9 @@ import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client.js";
 
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL not found");
+}
 const connectionString = `${process.env.DATABASE_URL}`;
 
 const adapter = new PrismaPg({ connectionString });
